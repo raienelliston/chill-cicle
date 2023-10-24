d411bd7cf71ffb4c465dcd8d263be906a7a6721a
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2023-10-24 08:30:46 -0700

    changed integrated libraries

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+#import github as gh
+from lib.github import gitCommands as gh
+import os
+
+def update():
+    if gh.check() == True:
+        gh.update()
+        update()
+        os.system("sudo reboot")
+    return True
+
 
 
 def setup():
@@ -6,3 +17,4 @@ def setup():
 def loop():
     None
 
+update()
